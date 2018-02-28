ed44b0ee6dea6d92f7214bce202be91ee2d1e14a
The Phenom <kmendki98@gmail.com>
2018-02-28 08:54:58 +0530

    First Commit

---
+++ AgroCulture_Project/db.php
@@ -0,0 +1,14 @@
+<?php
+
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "1708";
+    $dbName = "agroculture";
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+    if (!$conn)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+?>
